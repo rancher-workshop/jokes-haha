e3e0801f128017cc2e716b1cb28e472c5f077169
Guilherme Marques Rosa <marquesguilherme3@gmail.com>
2018-12-17 01:26:46 -0200

    Jokes

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+
+BASE_URL = 'https://api.icndb.com'
 
 
 @app.route('/hello')
@@ -9,6 +12,18 @@ def hello_route():
     return jsonify({
         'hello': 'world'
     })
+
+
+@app.route('/joke')
+def get_random_joke():
+    data = requests.get(BASE_URL + '/jokes/random').json()
+    return jsonify(data)
+
+
+@app.route('/jokes')
+def all_routes():
+    data = requests.get(BASE_URL + '/jokes').json()
+    return jsonify(data)
 
 
 if __name__ == "__main__":
